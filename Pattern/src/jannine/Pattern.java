c11a6e30bf370437fe154babb0b47e2007e020a1
PrachiiPatell <109415101+PrachiiPatell@users.noreply.github.com>
2023-04-14 15:36:30 +0530

    Add files via upload

---
+++ Pattern/src/jannine/Pattern.java
@@ -0,0 +1,17 @@
+package jannine;
+import java.util.Scanner;
+public class Pattern {
+	public static void main(String args[]) {
+		int num=3;
+		for( int i=1;i<=num;i++) {
+			
+			for(int j=1;j<=i;j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println(" ");
+				
+		}
+	}
+
+}
---
+++ Pattern/src/jannine/ReverseString.java
@@ -0,0 +1,13 @@
+package jannine;
+
+public class ReverseString {
+public static void main(String[] args) {
+	String s="Prachi Patel";
+	s.length();
+	String rev="";
+	for(int i=s.length()-1;i>=0;i--) {
+		rev=rev+s.charAt(i);
+	}
+	System.out.println(rev);
+}
+}
